8e891a2b79424a3034635273c1222838b370bafd
Raj Ladani <ladaniraj7@gmail.com>
2025-11-02 20:26:04 +0530

    test

--- src/app/(pages)/home/Home.tsx
+++ src/app/(pages)/home/Home.tsx
@@ -8,6 +8,7 @@ import SurpriseSection from "./SurpriseSection";
 import TestimonialSection from "./TestimonialSection";
 
 const HomePage = () => {
+    
     return (
         <>
             <BannerSection />
