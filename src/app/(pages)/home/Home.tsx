fa7f0888830c761c32c0789911d931cbf5d1f4bf
Raj Ladani <ladaniraj7@gmail.com>
2025-11-02 20:09:13 +0530

    latest

--- src/app/(pages)/home/Home.tsx
+++ src/app/(pages)/home/Home.tsx
@@ -10,7 +10,7 @@ import TestimonialSection from "./TestimonialSection";
 const HomePage = () => {
     return (
         <>
-            {/* <BannerSection /> */}
+            <BannerSection />
             <ServicesSection />
             <SurpriseSection />
             <HospitalDetails />
